c8c951e5b63b9f24e6946428fb0ba09923ac02d6
tetelows <tetelows@ualberta.ca>
2021-09-08 14:31:18 -0600

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,5 +1,6 @@
 import requests
 
-result = requests.get("https://www.google.com")
+#result = requests.get("https://www.google.com")
+result = requests.get("https://raw.githubusercontent.com/tetelows/CMPUT404_Lab1/main/lab1.py")
 print(result.content)
 #print(requests.__version__)
